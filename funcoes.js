24653baba04f07da3cf3e40fae0cc79f53952b3f
matheusclima <matheuslima1607@gmail.com>
2022-06-30 12:05:52 -0300

    Adição das funções salvar, buscar, listar e descrever

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,51 @@
 const cachorros = require('./database/cachorros.json');
+const fs = require('fs')
 
-function salvar(){}
+const salvar = () => {
+    let path = './database/cachorros.json'
+    let json = JSON.stringify(cachorros, null, 4)
+    fs.writeFileSync(path, json)
+}
 
-function buscar(id){}
+const buscar = id => {
+    let cachorroProcurado = cachorros.find(cachorro => cachorro.id === id)
+    if(cachorroProcurado) {
+        return cachorroProcurado
+    } else {
+        console.log("Cachorro não encontrado")
+    }
+}
+
+const listar = () => console.table(cachorros)
+
+const descrever = id => {
+    let cachorroProcurado = buscar(id)
+    if( cachorroProcurado ) {
+        console.table(cachorroProcurado)
+    } else {
+        console.log(`Não existe cachorro com o id ${id}`)
+    }
+} 
+
+const adicionar = (nomeDoCachorro, sexoDoCachorro, cachorroCastrado, dataNascimentoDoCachorro, pesoDoCachorro) => {
+    let novoCachorro = {
+        id: cachorros.length + 1,
+        nome: nomeDoCachorro,
+        sexo: sexoDoCachorro,
+        castrado: cachorroCastrado,
+        dataDeNascimento: dataNascimentoDoCachorro,
+        peso: pesoDoCachorro,
+        sexo: sexoDoCachorro,
+        vacinas: [],
+        servicos: [] 
+    }
+    cachorros.push(novoCachorro)
+    salvar(cachorros)
+}
 
 module.exports = {
-    
+    buscar,
+    listar,
+    descrever,
+    adicionar
 }
